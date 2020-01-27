4d11aa789b4930416da21c143e481a722db3038c
Scott Paige <scottpaige1@gmail.com>
2020-01-27 15:53:34 -0700

    First commit

---
+++ task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sonar-scanner -Dsonar.host.url=$SONAR_URL \ 
+-Dsonar.projectKey=$SONAR_PROJECT_KEY \ 
+-Dsonar.projectName=$SONAR_PROJECT_NAME-$SONAR_PROJECT_KEY \ 
+-Dsonar.login=$SONAR_TOKEN \
+-Dsonar.analysis.mode=preview \ 
+-Dsonar.github.repository=$GITHUB_REPOSITORY \ 
+-Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
