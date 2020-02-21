058a4f45b34d9c7430f001cfb91970d67a70b488
Scott Paige <scottpaige1@gmail.com>
2020-02-21 16:16:30 -0700

    Added cd to task script

--- task.sh
+++ task.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cd target
+
 sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.projectName=$SONAR_PROJECT_NAME-$SONAR_PROJECT_KEY -Dsonar.login=$SONAR_TOKEN -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$GITHUB_PULL_REQUEST -Dsonar.github.repository=$GITHUB_REPOSITORY -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
