19518dfad288017dd9c6bb2b9b511cadc3e5a31b
Adarsha Padyana <58205701+adarshasp87@users.noreply.github.com>
2020-03-27 20:29:21 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -7,4 +7,4 @@ SONAR_TOKEN="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/sonar-token)"
 
 cd target
 
-sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.projectName=$SONAR_PROJECT_NAME-$SONAR_PROJECT_KEY -Dsonar.login=$SONAR_TOKEN -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$GITHUB_PULL_REQUEST -Dsonar.github.repository=$GITHUB_REPOSITORY -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
+sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.projectName=$SONAR_PROJECT_NAME-$SONAR_PROJECT_KEY -Dsonar.java.coveragePlugin=jacoco -Dsonar.jacoco.reportPaths=target/coverage-reports/jacoco-unit.exec -Dsonar.jacoco.itReportPath=target/jacoco-it.exec -Dsonar.surefire.reportsPath=target/surefire-reports -Dsonar.javascript.jstestdriver.reportsPath=target/surefire-reports -Dsonar.junit.reportsPaths=target/surefire-reports -Dsonar.login=$SONAR_TOKEN -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$GITHUB_PULL_REQUEST -Dsonar.github.repository=$GITHUB_REPOSITORY -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
